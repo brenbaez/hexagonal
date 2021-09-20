7921a25c75c41d4f9b5921e32562c9e6206e1248
Leandro Tapia <jleandrotapia@gmail.com>
2021-09-20 20:52:18 -0300

    [IMPROVEMENT] Added cache decorator for CartRepository

---
+++ src/main/java/com/hexagonal/shop/cart/infrastructure/CachedCartRepository.java
@@ -0,0 +1,41 @@
+package com.hexagonal.shop.cart.infrastructure;
+
+import com.hexagonal.shop.cart.domain.Cart;
+import com.hexagonal.shop.cart.domain.CartRepository;
+import com.hexagonal.shop.shared.domain.valueobject.CartId;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository("cachedCartRepository")
+public class CachedCartRepository implements CartRepository {
+
+    private Map<String, Cart> cachedCarts = new HashMap<>();
+
+    private CartRepository cartRepository;
+
+    public CachedCartRepository(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+    @Override
+    public Optional<Cart> get(CartId cartId) {
+        var cachedCart = cachedCarts.get(cartId.value());
+        if (cachedCart != null)
+            return Optional.of(cachedCart);
+
+        Optional<Cart> cachedCartOpt = cartRepository.get(cartId);
+
+        cachedCartOpt.ifPresent(cart -> cachedCarts.put(cartId.value(), cart));
+
+        return cachedCartOpt;
+    }
+
+    @Override
+    public void save(Cart cart) {
+        cachedCarts.put(cart.getId().value(), cart);
+        cartRepository.save(cart);
+    }
+}
